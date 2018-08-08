62150484935ed5cf7bd894ed944ee23e633f8b64
OscarPicado <oscarpicadoortega@hotmail.com>
2018-08-08 15:11:49 -0600

    Botones longitud

--- js/app.js
+++ js/app.js
@@ -18,8 +18,49 @@
         lowercase_letter: 'a b c d e f g h i j k l m n o p q r s t u v w x y z'
     }
 
+    /*
     console.log(configuracion.caracteres);
     configuracion.caracteres = configuracion.caracteres + 1;
     console.log(configuracion.caracteres);
+
+    
+
+
+    /*
+
+    EVENTOS
+
+    */
+
+
+
+    app.addEventListener('submit', function(e) {
+
+        e.preventDefault();
+
+    });
+
+    app.elements.namedItem('btn-mas-uno').addEventListener('click', function() {
+        configuracion.caracteres++;
+        console.log(configuracion.caracteres);
+        inputCaracteres.value = configuracion.caracteres;
+
+    });
+
+    app.elements.namedItem('btn-menos-uno').addEventListener('click', function() {
+        if (configuracion.caracteres > 1) {
+            configuracion.caracteres--;
+            console.log(configuracion.caracteres);
+            inputCaracteres.value = configuracion.caracteres;
+        } else {
+
+        }
+
+    });
+
+
+    /*
+    FUNCIONES
+    */
 
 }())
