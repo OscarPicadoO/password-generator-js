ffc8a1e62a8a48ea8b2ec027a6e9d202404d50b8
OscarPicado <oscarpicadoortega@hotmail.com>
2018-08-08 20:30:35 -0600

    Interaccion Cadenas
    Medotos estarcados de generar y manipular las cadenas generadas,.
    
    Metodo de copiado y mensaje de copiado bajo temporizado.

--- js/app.js
+++ js/app.js
@@ -98,6 +98,11 @@
 
     });
 
+    app.elements.namedItem('input-password').addEventListener('click', function() {
+        copiarPassword();
+
+    });
+
 
     function generarPassword() {
         var caracteresUtilizados = '';
@@ -106,12 +111,40 @@
         for (propiedad in configuracion) {
             console.log('Ciclo');
             if (configuracion[propiedad] == true) {
-                caracteresUtilizados += caracteres[propiedad] + '   ';
+                caracteresUtilizados += caracteres[propiedad] + ' ';
 
             }
         }
-        console.log('Cadena seleccionada :' + caracteresUtilizados);
+        console.log('Cadena seleccionada:' + caracteresUtilizados);
+        caracteresUtilizados = caracteresUtilizados.trim();
+        caracteresUtilizados = caracteresUtilizados.split(' ');
+        console.log(caracteresUtilizados);
+
+        for (var r = 0; r < configuracion.caracteres; r++) {
+            password += caracteresUtilizados[Math.floor(Math.random() * caracteresUtilizados.length)];
+            /*caracteresUtilizados(Math.random());*/
+            /*console.log('Random' + (Math.floor(Math.random() * caracteresUtilizados.length)));
+             */
+            console.log(password);
+            app.elements.namedItem('input-password').value = password;
+            console.log(password);
+        }
+
     }
+
+    function copiarPassword() {
+        app.elements.namedItem('input-password').select();
+        document.execCommand('copy');
+        document.getElementById('alerta-copiado').classList.add('active');
+
+        setTimeout(function() {
+            document.getElementById('alerta-copiado').remove('active');
+            console.log('Timer');
+        }, 2000);
+
+    }
+
+    generarPassword();
 }())
 
 /*
@@ -120,4 +153,15 @@ app.elements.namedItem('btn-simbols').addEventListener('click', function () {
     this.classList.toggle('false');
     this.childNodes[1].classList.toggle('fa-check');
     this.childNodes[1].classList.toggle('fa-times');
-});*/
+});
+
+SPAMER
+
+
+for (var r = 0; r < configuracion.caracteres;) {
+            /*caracteresUtilizados(Math.random());
+console.log('Random' + caracteresUtilizados);
+        }
+
+
+*/
